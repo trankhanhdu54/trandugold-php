467a3b1949a3056b9831a47ebca3d878974952f6
Trần Khánh Dư <61740494+trankhanhdu54@users.noreply.github.com>
2023-06-27 14:47:44 +0700

    Update nhat-ki.php

--- nhat-ki.php
+++ nhat-ki.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="vi">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -14,7 +14,7 @@
     <meta property="og:site_name" content="Trần Khánh Dư">
     <meta property="og:description" content="Gold là một cặp nhân cách trong một tâm hồn. Một tâm hồn cho khoa học, một tâm hồn cho nghệ thuật.">
     <meta name="robots" content="index,follow">
-    <link type="image/png" rel="icon" href="../img/logo-gold.png">
+    <link type="image/png" rel="icon" href="img/logo-gold.png">
     <link rel="canonical" href="nhat-ki">
     
     <link type="text/css" rel="stylesheet" href="assets/css/gold/reset/v.1.0.0.min.css">
@@ -197,7 +197,7 @@
 
             <li class="column display_flex items_center content_center text_center padding_0 border_bottom_2 
             border_white ">
-                <a href="dich-vu" class="display_block padding_y_1">
+                <a href="gioi-thieu" class="display_block padding_y_1">
 
 
                     <div
